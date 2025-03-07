138396107530ad05ef0d2d1e48ff28e6971aacec
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-03-07 10:19:23 +0545

    understanding the condition

---
+++ Javascript-Basics/Understanding_conditionals/app.js
@@ -0,0 +1,12 @@
+const age = 18;
+if (age < 12) {
+  console.log("you are children");
+} else if (age <= 19) {
+  console.log("you are teenager");
+} else if (age > 20) {
+  console.log("you are adult");
+} else if (age > 60) {
+  console.log("you are senior citizen");
+} else {
+  console.log("Bonus person");
+}
